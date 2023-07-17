6e36368fb345074eb13fc38eb89265b4f32b44d5
paperbenni <paperbenni@gmail.com>
2023-07-17 20:36:50 +0200

    progress gemacht bruda

--- lena2048.cpp
+++ lena2048.cpp
@@ -1,30 +1,133 @@
 #include <algorithm>
+#include <cstdlib>
 #include <curses.h>
 #include <ncurses.h>
 using namespace std;
 
 // This is a 2048 game
 
-void createGrid(WINDOW *grid[4][4]) {
+enum Direction { UP, DOWN, LEFT, RIGHT };
+
+void createBoard(WINDOW *board[4][4]) {
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
-      grid[i][j] = newwin(5, 8, 20 + i * 5, 20 + j * 8);
-      box(grid[i][j], 0, 0);
-      wrefresh(grid[i][j]);
+      board[i][j] = newwin(5, 8, 20 + i * 5, 20 + j * 8);
+      box(board[i][j], 0, 0);
+      wrefresh(board[i][j]);
     }
   }
 }
 
-void renderGrid(WINDOW *grid[4][4], int board[4][4]) {
+void renderGrid(WINDOW *board[4][4], int grid[4][4]) {
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
-      wclear(grid[i][j]);
-      box(grid[i][j], 0, 0);
-      if (board[i][j] != 0) {
-        mvwprintw(grid[i][j], 2, 2, "%d", board[i][j]);
+      wclear(board[i][j]);
+      box(board[i][j], 0, 0);
+      if (grid[i][j] != 0) {
+        mvwprintw(board[i][j], 2, 2, "%d", grid[i][j]);
       }
-      wrefresh(grid[i][j]);
+      wrefresh(board[i][j]);
     }
+  }
+}
+
+void stackAtTheEnd(int column[4]) {
+  int stack[4];
+  int stackIndex = 0;
+  for (int i = 0; i < 4; i++) {
+    if (column[i] != 0) {
+      stack[stackIndex] = column[i];
+      stackIndex++;
+    }
+  }
+  for (int i = 0; i < 4; i++) {
+    column[i] = stack[i];
+  }
+}
+
+void processMove(int column[4]) {
+  stackAtTheEnd(column);
+  for (int i = 0; i < 3; i++) {
+    if (column[i] == column[i + 1]) {
+      column[i] *= 2;
+      column[i + 1] = 0;
+    }
+  }
+  stackAtTheEnd(column);
+}
+
+void transpose(int grid[4][4]) {
+  int temp;
+  for (int i = 0; i < 4; i++) {
+    for (int j = i; j < 4; j++) {
+      temp = grid[i][j];
+      grid[i][j] = grid[j][i];
+      grid[j][i] = temp;
+    }
+  }
+}
+
+void spawnNewNumber(int grid[4][4]) {
+  int x, y;
+  do {
+    x = rand() % 4;
+    y = rand() % 4;
+  } while (grid[x][y] != 0);
+  grid[x][y] = 2;
+}
+
+// TODO: test this
+bool hasLost(int grid[4][4]) {
+  int temp[4][4];
+  copy(&grid[0][0], &grid[0][0] + 16, &temp[0][0]);
+  for (int i = 0; i < 4; i++) {
+    processMove(temp[i]);
+  }
+  transpose(temp);
+  for (int i = 0; i < 4; i++) {
+    processMove(temp[i]);
+  }
+  transpose(temp);
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (temp[i][j] == temp[i][j + 1]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+void processGridMove(int grid[4][4], Direction direction) {
+  switch (direction) {
+  case UP:
+    transpose(grid);
+    for (int i = 0; i < 4; i++) {
+      processMove(grid[i]);
+    }
+    transpose(grid);
+    break;
+  case DOWN:
+    transpose(grid);
+    for (int i = 0; i < 4; i++) {
+      reverse(grid[i], grid[i] + 4);
+      processMove(grid[i]);
+      reverse(grid[i], grid[i] + 4);
+    }
+    transpose(grid);
+    break;
+  case LEFT:
+    for (int i = 0; i < 4; i++) {
+      processMove(grid[i]);
+    }
+    break;
+  case RIGHT:
+    for (int i = 0; i < 4; i++) {
+      reverse(grid[i], grid[i] + 4);
+      processMove(grid[i]);
+      reverse(grid[i], grid[i] + 4);
+    }
+    break;
   }
 }
 
@@ -32,42 +135,47 @@ int main() {
 
   initscr();
 
-  WINDOW *grid[4][4];
-  createGrid(grid);
+  WINDOW *board[4][4];
+  createBoard(board);
+
   refresh();
 
-  WINDOW *win = newwin(5, 8, 20, 20);
-  box(win, 0, 0);
-  mvwprintw(win, 1, 1, "Hello there");
-  wrefresh(win);
-  curs_set(0);
-
   while (true) {
-    int ch = wgetch(win);
-    wclear(win);
-
-    box(win, 0, 0);
-    if (ch == 'q') {
-      break;
-    }
-
-    switch(ch) {
+    int grid[4][4] = {{0}};
+    spawnNewNumber(grid);
+    spawnNewNumber(grid);
+    renderGrid(board, grid);
+    bool quit = false;
+    while (!hasLost(grid) || !quit) {
+      int ch = getch();
+      switch (ch) {
       case 'w':
-        mvwprintw(win, 2, 2, "2048");
+        processGridMove(grid, UP);
         break;
       case 's':
-        mvwprintw(win, 2, 2, "1024");
-        break;
-      case 'd':
-        mvwprintw(win, 2, 2, "6");
+        processGridMove(grid, DOWN);
         break;
       case 'a':
-        mvwprintw(win, 2, 2, "128");
+        processGridMove(grid, LEFT);
         break;
+      case 'd':
+        processGridMove(grid, RIGHT);
+        break;
+      case 'q':
+        quit = true;
+        break;
+      default:
+        continue;
+      }
+      spawnNewNumber(grid);
+      renderGrid(board, grid);
     }
-    wrefresh(win);
+    mvprintw(10, 10, "You lost!");
+    refresh();
+    getch();
   }
 
   endwin();
   return 0;
 }
+
